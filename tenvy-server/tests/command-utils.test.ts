2665642e3e42c1c5a1b7a7099acb23f35ff975d0
Xein <100153223+XeinTDM@users.noreply.github.com>
2025-10-22 20:57:06 +0100

    Fix handling of empty quoted tokens in command parser

---
+++ tenvy-server/tests/command-utils.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+import { splitCommandLine } from '../../shared/utils/command.ts';
+
+describe('splitCommandLine', () => {
+        it('preserves empty arguments created by double quotes', () => {
+                expect(splitCommandLine(['cmd', '""', 'arg'].join(' '))).toEqual(['cmd', '', 'arg']);
+        });
+
+        it('preserves multiple adjacent empty arguments', () => {
+                expect(splitCommandLine(['""', '""'].join(' '))).toEqual(['', '']);
+        });
+});
